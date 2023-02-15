4de569470119723a6b7f34de355e476f79259b96
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-02-15 16:53:18 -0300

    Incrementado o metodo switch

--- src/test/java/intro/Medidas.java
+++ src/test/java/intro/Medidas.java
@@ -10,9 +10,47 @@ public class Medidas {
     // 3.2 - Métodos e Funções
     public static void main(String[] args){
         System.out.println("Cálculo de Areas");
+        // Condicional = Verificar uma condição - Fazer uma pergunta para uma pessoa, um hardware ou software
+        // Switch = Selecionar o comportamento do programa conforma a escolha da pessoa ou software
 
-        calcularAreaModoExtenso();
-        calcularAreaModoCompacto();
+        String opcao = "ifSimples";
+
+        switch (opcao){
+            case "ifSimples":
+                System.out.println("Você escolheu executar o método ifSimples");
+                ifSimples();
+                break;
+            case "compacto":
+                System.out.println("Você escolheu executar o método calcularAreaModoCompacto");
+                calcularAreaModoCompacto();
+                break;
+            case "extenso":
+                System.out.println("Você escolheu executar o método calcularAreaModoExtenso");
+                calcularAreaModoExtenso();
+                break;
+            default:
+                System.out.println("Você não escolheu nenhum dos metodos");
+                break;
+        }
+
+
+    }
+
+    public static void ifSimples(){
+        // Condicional = Verificar uma condição - Fazer uma pergunta para uma pessoa, um hardware ou software
+
+        // if = se
+        // else = senão
+
+        String modo = "melancia";
+
+        if (modo == "curto"){
+            calcularAreaModoCompacto();
+        }
+        else{
+            calcularAreaModoExtenso();
+        }
+
 
     }
 
@@ -26,7 +64,7 @@ public class Medidas {
         comprimento = 6; // comprimento recebe 3 unidades
 
         resultado = largura * comprimento;
-        System.out.println("Para a largura de " + largura + "m e o comprimento de " + comprimento + "m a área total é de "
+        System.out.println("[MODO EXTENSO] Para a largura de " + largura + "m e o comprimento de " + comprimento + "m a área total é de "
                 + resultado + " m²");
 
     }
@@ -35,7 +73,7 @@ public class Medidas {
         int largura = 4;
         int comprimento = 3;
 
-        System.out.println("Para a largura de " + largura + "m e o comprimento de " + comprimento + "m a área total é de "
+        System.out.println("[MODO COMPACTO] Para a largura de " + largura + "m e o comprimento de " + comprimento + "m a área total é de "
                 + largura * comprimento + " m²");
 
     }
